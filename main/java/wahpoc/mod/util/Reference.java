1720d64c4a58499b1a0819370b0eaedaa0394a9a
Muniversal Arts <55272259+MuniversalRabat@users.noreply.github.com>
2020-12-07 21:47:01 -0700

    Delete Reference.java

--- main/java/wahpoc/mod/util/Reference.java
+++
@@ -1,9 +0,0 @@
-package wahpoc.mod.util;
-
-public class Reference {
-	public static final String MODID = "wahpoc";
-	public static final String NAME = "WAHpocalypse Mod";
-	public static final String VERSION = "1.0";
-	public static final String CLIENT = "wahpoc.mod.proxy.ClientProxy";
-	public static final String COMMON = "wahpoc.mod.proxy.CommonProxy";
-}
